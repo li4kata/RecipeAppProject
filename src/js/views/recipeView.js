df7162d89735287a8d50a147108a0a90ff07b096
ilia iliev <lichkata1992@gmail.com>
2022-09-29 22:12:50 +0100

    Fixing the issue with fractional which couse big problem with the searching function of the app

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import {Fraction} from 'fractional';
+import { numberToFraction } from '../helpers';
+
 
 
 class RecipeView extends View{
@@ -121,7 +122,7 @@ class RecipeView extends View{
                 <svg class="recipe__icon">
                   <use href="${icons}.svg#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity ? new Fraction( ing.quantity ).toString() : ''}</div>
+                <div class="recipe__quantity">${ing.quantity? numberToFraction(ing.quantity).toString() : ''}</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
                   ${ing.description}
